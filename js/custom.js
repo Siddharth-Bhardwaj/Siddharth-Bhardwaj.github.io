6145c25ca49c36131bef2f70b37b09e27a7bff9a
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2020-10-15 17:32:53 +0530

    small tweaks, added map in contact page

--- js/custom.js
+++ js/custom.js
@@ -1,169 +1,164 @@
-
- /* jQuery Pre loader
+/* jQuery Pre loader
   -----------------------------------------------*/
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+$(window).load(function () {
+    $(".preloader").fadeOut(1000); // set duration in brackets
 });
 
-
- /* Google Map
+/* Google Map
 -----------------------------------------------*/
-var map = '';
-var center;
+// var map = '';
+// var center;
 
-function initialize() {
-    var mapOptions = {
-      zoom: 16,
-      center: new google.maps.LatLng(13.758468, 100.567481),
-      scrollwheel: false
-    };
-  
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+// function initialize() {
+//     var mapOptions = {
+//       zoom: 16,
+//       center: new google.maps.LatLng(13.758468, 100.567481),
+//       scrollwheel: false
+//     };
 
-    google.maps.event.addDomListener(map, 'idle', function() {
-        calculateCenter();
-    });
-  
-    google.maps.event.addDomListener(window, 'resize', function() {
-        map.setCenter(center);
-    });
-}
+//     map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 
-function calculateCenter() {
-  center = map.getCenter();
-}
+//     google.maps.event.addDomListener(map, 'idle', function() {
+//         calculateCenter();
+//     });
 
-function loadGoogleMap(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
+//     google.maps.event.addDomListener(window, 'resize', function() {
+//         map.setCenter(center);
+//     });
+// }
 
-$(function(){
-  loadGoogleMap();
-});
+// function calculateCenter() {
+//   center = map.getCenter();
+// }
 
+// function loadGoogleMap(){
+//     var script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+//     document.body.appendChild(script);
+// }
+
+// $(function(){
+//   loadGoogleMap();
+// });
 
 /* Istope Portfolio
 -----------------------------------------------*/
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+    if ($(".iso-box-wrapper").length > 0) {
+        var $container = $(".iso-box-wrapper"),
+            $imgs = $(".iso-box img");
 
-  if ( $('.iso-box-wrapper').length > 0 ) { 
+        $container.imagesLoaded(function () {
+            $container.isotope({
+                layoutMode: "fitRows",
+                itemSelector: ".iso-box",
+            });
 
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
-
-      $container.imagesLoaded(function () {
-
-        $container.isotope({
-        layoutMode: 'fitRows',
-        itemSelector: '.iso-box'
+            $imgs.load(function () {
+                $container.isotope("reLayout");
+            });
         });
 
-        $imgs.load(function(){
-          $container.isotope('reLayout');
-        })
+        //filter items on button click
 
-      });
+        $(".filter-wrapper li a").click(function () {
+            var $this = $(this),
+                filterValue = $this.attr("data-filter");
 
-      //filter items on button click
+            $container.isotope({
+                filter: filterValue,
+                animationOptions: {
+                    duration: 750,
+                    easing: "linear",
+                    queue: false,
+                },
+            });
 
-      $('.filter-wrapper li a').click(function(){
+            // don't proceed if already selected
 
-          var $this = $(this), filterValue = $this.attr('data-filter');
+            if ($this.hasClass("selected")) {
+                return false;
+            }
 
-      $container.isotope({ 
-        filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
+            var filter_wrapper = $this.closest(".filter-wrapper");
+            filter_wrapper.find(".selected").removeClass("selected");
+            $this.addClass("selected");
 
-      // don't proceed if already selected 
-
-      if ( $this.hasClass('selected') ) { 
-        return false; 
-      }
-
-      var filter_wrapper = $this.closest('.filter-wrapper');
-      filter_wrapper.find('.selected').removeClass('selected');
-      $this.addClass('selected');
-
-        return false;
-      }); 
-
-  }
-
+            return false;
+        });
+    }
 });
 
-
- /* Navigation Bar
+/* Navigation Bar
   -----------------------------------------------*/
-$(document).ready(function() { 
+$(document).ready(function () {
     "use strict";
 
     // Navbar Sticky
 
-    (function() {
+    (function () {
         var docElem = document.documentElement,
             didScroll = false,
             stickynav = 50;
-            document.querySelector( '.nav-container' );
+        document.querySelector(".nav-container");
         function init() {
-            window.addEventListener( 'scroll', function() {
-                if( !didScroll ) {
-                    didScroll = true;
-                    setTimeout( scrollPage, 50 );
-                }
-            }, false );
+            window.addEventListener(
+                "scroll",
+                function () {
+                    if (!didScroll) {
+                        didScroll = true;
+                        setTimeout(scrollPage, 50);
+                    }
+                },
+                false
+            );
         }
-        
+
         function scrollPage() {
             var sy = scrollY();
-            if ( sy >= stickynav ) {
-                $( '.nav-container' ).addClass('sticky');
-            }
-            else {
-                $( '.nav-container' ).removeClass('sticky');
+            if (sy >= stickynav) {
+                $(".nav-container").addClass("sticky");
+            } else {
+                $(".nav-container").removeClass("sticky");
             }
             didScroll = false;
         }
-        
+
         function scrollY() {
             return window.pageYOffset || docElem.scrollTop;
-        }        
-        init();        
+        }
+        init();
     })();
-
 });
 
-
-$(document).ready(function(){
-            
+$(document).ready(function () {
     "use strict";
 
-    $('.menu-container').each(function(index) {
-        $(this).find('.circle').attr('menu-link', index);
-        $(this).find('.list-menu').clone().appendTo('body').attr('menu-link', index);
+    $(".menu-container").each(function (index) {
+        $(this).find(".circle").attr("menu-link", index);
+        $(this)
+            .find(".list-menu")
+            .clone()
+            .appendTo("body")
+            .attr("menu-link", index);
     });
 
-    $('.menu-container .circle').click(function() {
-        var linkedVideo = $('section').closest('body').find('.list-menu[menu-link="' + $(this).attr('menu-link') + '"]');
-        linkedVideo.toggleClass('reveal-modal');
-       
+    $(".menu-container .circle").click(function () {
+        var linkedVideo = $("section")
+            .closest("body")
+            .find('.list-menu[menu-link="' + $(this).attr("menu-link") + '"]');
+        linkedVideo.toggleClass("reveal-modal");
     });
 
-    $('section').closest('body').find('.close-iframe').click(function() {
-        $(this).closest('.list-menu').toggleClass('reveal-modal');
-    });
-    
+    $("section")
+        .closest("body")
+        .find(".close-iframe")
+        .click(function () {
+            $(this).closest(".list-menu").toggleClass("reveal-modal");
+        });
 
-  /* wow
+    /* wow
   -------------------------------*/
-  new WOW({ mobile: false }).init();
-
-  });
-
+    new WOW({ mobile: false }).init();
+});
